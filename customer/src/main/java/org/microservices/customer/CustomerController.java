108f79025e2c1779f4732187ec703bed3b72240f
shadai-rafael <shadai.lopez.garcia@gmail.com>
2022-09-09 20:03:36 -0500

    Creation of fraud and customer microservice
    Signed-off-by: shadai-rafael <shadai.lopez.garcia@gmail.com>

---
+++ customer/src/main/java/org/microservices/customer/CustomerController.java
@@ -0,0 +1,39 @@
+/*
+MIT License
+Copyright (c) 2022 shadai-rafael
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+package org.microservices.customer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("api/v1/customers")
+public record CustomerController (CustomerService customerService){
+
+    @PostMapping
+    public void registerCostumer(@RequestBody CustomerRequest customer){
+        log.info("new costumer registration {}", customer);
+        customerService.addCustomer(customer);
+    }    
+    
+}
---
+++ customer/src/test/java/org/microservices/costumer/AppTest.java
@@ -0,0 +1,18 @@
+package org.microservices;
+
+//import static org.junit.Assert.assertTrue;
+
+//import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+
+
+    public void shouldAnswerWithTrue()
+    {
+        //assertTrue( true );
+    }
+}
